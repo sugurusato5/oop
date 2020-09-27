7d2a2bb1c03e009a72df5b4a25535eaf1dadbb8d
sugurusato5 <jt.ag.dm.2782@gmai.com>
2020-09-27 15:05:10 +0900

    課題質問

--- index.php
+++ index.php
@@ -52,42 +52,55 @@ class Car
 }
 
 
-// 以下を実行した時にエラーがでないようにして下さい。
+/// 以下にCarクラスを継承したTaxiクラスを記述して下さい
+class Taxi extends Car 
+{
+    public $passenger;
+    
+    public function infomation()
+    {
+        echo '車の車種:' . $this->name . '、' .  '車体番号:' . $this->number . '、' . 'カラー:' . $this->color . '、' . '乗車人数は' . $this->passenger . '人です。<br>';
+    }
+    
+    public function pickUp($passenger)
+    {
+        $this->passenger = $passenger;
+    }
+
+    public function lower($number)
+    {
+        if($this->passenger - $number >= 0) {
+            echo $number . '人降車しました。';
+        } else {
+            echo '降車人数に誤りがあります。';
+        }
+        return $this->passenger;
+    }
+}
+
+// 以下を実行した時にエラーがでないようにして下さい
 // Carクラスのインスタンスを生成
 // 引数 : 車名、車体番号、カラー
 $car1 = new Car('ランクル', 100, 'black');
 
-// 車名(車種)を取得
-echo $car1->getName();
-echo '<br>';
-
-// 車体番号を取得
-echo $car1->getNumber();
-echo '<br>';
-
-// カラーを取得
-echo $car1->getColor();
-echo '<br>';
-
-// 車の情報表示を表示
+// 車の情報を表示
 echo $car1->infomation();
 echo '<br>';
+echo '<hr>';
 
-// 車名(車種)を更新
-$car1->setName('アルファード');
-echo $car1->getName();
+// Taxiクラスのインスタンスを生成
+$taxi1 = new Taxi('クラウンのタクシー', 222, 'black');
+
+// 3人乗車
+$taxi1->pickUp(3);
+
+// タクシーの情報表示を表示
+echo $taxi1->infomation();
 echo '<br>';
 
-// 車体番号を取得
-$car1->setNumber(200);
-echo $car1->getNumber();
+// 2人降車
+$taxi1->lower(2);
 echo '<br>';
 
-// カラーを取得
-$car1->setColor('red');
-echo $car1->getColor();
-echo '<br>';
-
-// 車の情報表示を表示
-echo $car1->infomation();
-echo '<br>';
+// 2人降車
+$taxi1->lower(2);
